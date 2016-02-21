be84c7446dcf75d64faf29f9e6fbcee07c640eac
Sevag Nadjarian <sevnadj@umich.edu>
2016-02-20 21:26:20 -0500

    port 80

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ mysql.init_app(app)
 # if you dont, gunicorn will ignore it anyway
 if __name__ == '__main__':
     # listen on external IPs
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
