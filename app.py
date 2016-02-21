aa776d72e806aa2546b27db73a29edf998db557b
Sevag Nadjarian <sevnadj@umich.edu>
2016-02-20 21:38:56 -0500

    Fixed the routing in index, and took out sql in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask.ext.mysqldb import MySQL
+#from flask.ext.mysqldb import MySQL
 
 import controllers
 
@@ -8,14 +8,14 @@ app = Flask(__name__, template_folder='views')
 app.register_blueprint(controllers.main)
 app.register_blueprint(controllers.muscle)
 
-mysql = MySQL()
-app.config['MYSQL_USER'] = 'root'
+#mysql = MySQL()
+#app.config['MYSQL_USER'] = 'root'
 # app.config['MYSQL_PASSWORD'] = 'hello'
-app.config['MYSQL_PASSWORD'] = 'doodle'
-mysql.init_app(app)
+#app.config['MYSQL_PASSWORD'] = 'doodle'
+#mysql.init_app(app)
 
 # comment this out using a WSGI like gunicorn
 # if you dont, gunicorn will ignore it anyway
 if __name__ == '__main__':
     # listen on external IPs
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
