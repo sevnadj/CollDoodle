1e9330363d2ba8a5c150ab804b678034b39f6b43
Acaruso <acarusoa@umich.edu>
2016-02-23 09:52:33 -0500

    added error route

--- controllers/main.py
+++ controllers/main.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__, template_folder='views')
 @main.route('/')
 def main_route():
     return render_template("index.html")
+    
+@error.route('/error')
+def error_route():
+	return render_template("error.html")
