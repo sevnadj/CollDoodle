5572da90708043cde060416809eba2db03da2893
Sevag Nadjarian <sevnadj@umich.edu>
2016-02-23 10:00:28 -0500

    Added more doodles to UI, non-functioning

--- controllers/main.py
+++ controllers/main.py
@@ -1,6 +1,7 @@
 from flask import *
 
 main = Blueprint('main', __name__, template_folder='views')
+error = Blueprint('error', __name__, template_folder='views')
 
 @main.route('/')
 def main_route():
